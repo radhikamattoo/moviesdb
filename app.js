b55c1256f04feafccce4a9bc4bf4d113294c364b
Radhika Mattoo <rm3485@nyu.edu>
2016-03-28 14:56:33 -0400

    added clarifying comments

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
+/*
+Radhika Mattoo, rm3485@nyu.edu
+Applied Internet Tech Spring 2016
+Homework 5
+*/
 require('./db');
 var express = require('express');
 var app = express();
 //mongoose
 var mongoose = require('mongoose');
 var Movie = mongoose.model('Movie');
+
 //templating
 var handlebars = require('express-handlebars')
 	.create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
 //access request body
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+//GET request for list of movies, with certain director (if specified)
 app.get('/movies', function(req, res){
 	var findObject = {};
 	if(req.query.director !== undefined && req.query.director !== ""){
@@ -27,10 +34,12 @@ app.get('/movies', function(req, res){
   });
 });
 
+//GET request for the add movie form
 app.get('/movies/add', function(req, res){
 	res.render('addMovie');
 });
 
+//POST request to add a movie, redirects to /movies with new movie added to db
 app.post('/movies/add', function(req,res){
 	var newMovie = new Movie({
 		title : req.body.title,
--- db.js
+++ db.js
@@ -1,10 +1,17 @@
-
+/*
+Radhika Mattoo, rm3485@nyu.edu
+Applied Internet Tech Spring 2016
+Homework 5
+*/
 var mongoose = require('mongoose');
 
+//create a schema
 var Movie = new mongoose.Schema({
   title: String,
   director:String,
   year:Number
 });
+
+//register the schema and connect to the database
 mongoose.model('Movie', Movie);
 mongoose.connect('mongodb://localhost/hw05');
